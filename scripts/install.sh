2b3e67ae9a9c25b1ddcd89a7721a05c9e3fb48c1
mc-b <marcel.bernet@ch-open.ch>
2019-06-18 20:22:16 +0200

    typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@
 docker pull misegr/make
 docker pull misegr/mbed-cli
 docker pull misegr/maven
-kubectl apply -f hhttps://raw.githubusercontent.com/mc-b/duk/master/registry/nexus3.yaml
+kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/registry/nexus3.yaml
 
 # Jupyter Scripte etc. Allgemein verfuegbar machen
 cp -rpv [0?]*/* /data/src/
