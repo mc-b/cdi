aab50d1c0e671eb62753d98b95a10decb0137ea3
mc-b <marcel.bernet@ch-open.ch>
2021-01-14 10:26:51 +0100

    make und maven durch Eclipse Theia ersetzt

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,11 +3,12 @@
 #	Installationsscript cdi
 
 # Compiler und DevOps Umgebungen
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/compiler/make.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/compiler/maven.yaml
+# kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/compiler/make.yaml
+# kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/compiler/maven.yaml
 kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/registry/nexus3.yaml
 kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/devops/gogs.yaml
 kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/devops/jenkins.yaml
+kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/devops/eclipse-theia.yaml
 
 
 # Jupyter Scripte etc. Allgemein verfuegbar machen
