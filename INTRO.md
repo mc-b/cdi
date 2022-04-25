d4fd01daffd1afa682e3645bb4d3be6f0cbd1485
mc-b <marcel.bernet@ch-open.ch>
2022-04-25 17:46:39 +0200

    Optimiert

--- INTRO.md
+++ INTRO.md
@@ -3,13 +3,10 @@ Kubernetes
 
 Umgebung zum Kurs: [DevOps Engineering Practices & Tools (CDI)](https://github.com/mc-b/cdi).
 
-Applikationen
--------------
+Services
+--------
 
 * https://${fqdn}:8443 - Kubernetes Dashboard (kein Token notwendig, Überspringen drücken)
 * http://${fqdn}:32400 - Entwicklungsumgebung Eclipse Theia
 * http://${fqdn}:32100 - CI/CD Jenkins (User/PW: admin)
-* http://${fqdn}:32188 - Jupyter Notebooks
-* http://${fqdn}:32400 - Gogs, einfacher GitHub Clone (SQLite mit /data Verzeichnis Konfigurieren)
-* http://${fqdn}:32511 - Nexus3, Repository Manager
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,16 +2,16 @@
 #
 #	Installationsscript cdi
 
-# Compiler und DevOps Umgebungen
-# kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/compiler/make.yaml
-# kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/compiler/maven.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/registry/nexus3.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/devops/gogs.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/devops/jenkins.yaml
+# DevOps Umgebungen
+
 kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/devops/eclipse-theia.yaml
 
+microk8s helm3 repo add jenkins https://charts.jenkins.io
+microk8s helm3 repo update
+microk8s helm3 install jenkins jenkins/jenkins
 
 # Jupyter Scripte etc. Allgemein in Eclipse Theia verfuegbar machen
 mkdir -p /data/project/src
 mkdir -p /data/project/cdi
 cp -rpv [0?]* /data/project/cdi/
+
