eff27f21fa61a44ea5b5ca07b64b43a31a1f0c57
mc-b <marcel.bernet@ch-open.ch>
2022-04-26 19:05:38 +0200

    Jenkins mit eigenen Values installieren

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,7 @@ kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/devops/eclips
 
 microk8s helm3 repo add jenkins https://charts.jenkins.io
 microk8s helm3 repo update
+microk8s kubectl create namespace jenkins
 microk8s helm3 upgrade --install jenkins --namespace jenkins -f scripts/values.yaml jenkins/jenkins
 
 # Jupyter Scripte etc. Allgemein in Eclipse Theia verfuegbar machen
