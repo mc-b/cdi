0e126d9b44cdb5f0e50ce66e7c5437f12847ed7c
mc-b <marcel.bernet@ch-open.ch>
2022-04-27 18:16:49 +0200

    Lokale GitLab Umgebung

---
+++ 08-Tools/main.tf
@@ -0,0 +1,31 @@
+###
+#   Kubernetes Umgebung
+#
+
+module "gitlab" {
+
+  #source     = "./terraform-lerncloud-module"
+  source     = "git::https://github.com/mc-b/terraform-lerncloud-multipass"
+  #source     = "git::https://github.com/mc-b/terraform-lerncloud-maas"
+  #source     = "git::https://github.com/mc-b/terraform-lerncloud-lernmaas"  
+  #source     = "git::https://github.com/mc-b/terraform-lerncloud-aws"
+  #source     = "git::https://github.com/mc-b/terraform-lerncloud-azure" 
+
+  module      = "gitlab-${var.host_no}-${terraform.workspace}"
+  description = "GitLab"
+  userdata    = "cloud-init.yaml"
+
+  cores   = 4
+  memory  = 8
+  storage = 64
+  # SSH, Kubernetes, NFS
+  ports   = [22, 6443, 111, 2049]
+
+  # MAAS Server Access Info
+  url = var.url
+  key = var.key
+  vpn = var.vpn
+}
+
+
+
